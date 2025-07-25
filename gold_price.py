249494fe40ea8f0708e24ac6a3fc8c3bf519cc40
Alen Li <f2855631@gmail.com>
2025-07-26 01:00:28 +0800

    Update gold_price.py

--- gold_price.py
+++ gold_price.py
@@ -2,11 +2,10 @@ import requests
 import json
 import os
 
-
 Gold_API_url = "https://www.goldapi.io/api/XAU/USD"
-headers = {"x-access-token":"goldapi-4y76119mdiwlgh2-io"}
-response = requests.get(Gold_API_url,headers=headers)
+headers = {"x-access-token": os.getenv("GOLDAPI_KEY")}
+response = requests.get(Gold_API_url, headers=headers)
 output = response.json()
 
-with open ("Gold_price.json", "w", encoding="utf-8") as f:
-    json.dump(output,f,ensure_ascii=False,indent=1)
+with open("Gold_price.json", "w", encoding="utf-8") as f:
+    json.dump(output, f, ensure_ascii=False, indent=1)
